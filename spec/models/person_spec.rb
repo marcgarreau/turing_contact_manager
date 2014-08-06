3f04e40f1b99070b1bf557b1f067257a1391241b
Marc Garreau <marcdgarreau@gmail.com>
2014-08-06 15:13:50 -0600

    Sets up email model and relationships

--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -8,15 +8,22 @@ RSpec.describe Person, :type => :model do
   it 'is valid' do  	
   	expect(person).to be_valid
   end
+  
   it 'is invalid without a first name' do
   	person.first_name = nil
   	expect(person).not_to be_valid
   end
+  
   it 'is invalid without a last name' do
   	person.last_name = nil
   	expect(person).not_to be_valid
   end
+  
   it 'has an array of phone numbers' do
   	expect(person.phone_numbers).to eq([])
+  end
+
+  it 'has an array of email addresses' do
+    expect(person.email_addresses).to eq([])
   end
 end
