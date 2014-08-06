69c56ed9549b34f3ae7a78d9196a9517a3284969
Marc Garreau <marcdgarreau@gmail.com>
2014-08-06 15:52:52 -0600

    Adds more model tests for email, and requires a person_id

--- spec/models/email_address_spec.rb
+++ spec/models/email_address_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe EmailAddress, :type => :model do
 	let(:email_address) do
-		EmailAddress.new(address: 'abc@example.com')
+		EmailAddress.new(address: 'abc@example.com', person_id: 1)
 	end
 
 	it 'is valid' do
@@ -12,5 +12,9 @@ RSpec.describe EmailAddress, :type => :model do
 	it 'is invalid without an address' do
 		email_address.address = nil
 		expect(email_address).not_to be_valid
+	end
+
+	it 'is associated with a person' do
+		expect(email_address).to respond_to(:person)
 	end
 end
