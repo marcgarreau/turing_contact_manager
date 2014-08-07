18d7470de7f9d11d6ba86b8d9663159d0a9653c3
Marc Garreau <marcdgarreau@gmail.com>
2014-08-07 10:35:31 -0600

    Cleans up phone number controller spec

--- spec/models/phone_number_spec.rb
+++ spec/models/phone_number_spec.rb
@@ -11,6 +11,11 @@ RSpec.describe PhoneNumber, :type => :model do
   	phone_number.number = nil
   	expect(phone_number).to_not be_valid
   end
+
+  it 'is invalid without a contact id' do
+    phone_number.contact_id = nil
+    expect(phone_number).to_not be_valid
+  end
   
   it 'is associated with a contact' do
   	expect(phone_number).to respond_to(:contact)
